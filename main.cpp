0435108ce295f67ea8007831d42252fdcbb45513
claytongentry <claytongentry2017@u.northwestern.edu>
2016-11-05 07:41:44 -0500

    add main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * JeezyAI is a rap bot that emulates Young Jeezy.
+ *
+ * Given a bar, JeezyAI will return a rhyme on beat. The AI is trained on a
+ * bank of Jeezy lyrics, so it spits like Jeezy.
+ *
+ * Cameron Matson and Clayton Gentry
+ */
+
+ #include <iostream>
+ #include <string>
+
+int main(int argc, char *argv[]) {
+  std::string bar;
+
+  std::cout << "Gimme a bar\n";
+  std::getline(std::cin, bar);
+
+  return 0;
+}
