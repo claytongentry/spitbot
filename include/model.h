cd21af19a10b8d05d2e421fde3c3047e5ee473d4
claytongentry <claytongentry2017@u.northwestern.edu>
2016-11-05 09:20:10 -0500

    add model definition

---
+++ include/model.h
@@ -0,0 +1,27 @@
+/*
+ * model.h
+ *
+ * The model used to power the AI's decision making is an adjacency list
+ * of all of Young Jeezy's lyrics.
+ *
+ * The adjacency list represents an n x n matrix of all the lyrics.
+ * The element at the intersection of a word on the x-axis and a word
+ * on the y axis represents the number of times the y-axis word preceded
+ * the x-axis word in Jeezy's songs.
+ */
+
+#ifndef model_H
+#define model_H
+
+using namespace std;
+
+class model {
+  public:
+    void build_list();
+    int get_frequency(string, string);
+
+  private:
+    int frequency;
+    string word;
+};
+#endif
---
+++ src/model.cpp
@@ -0,0 +1,15 @@
+/*
+ * Model Class Implementation
+ */
+
+ #include <iostream>
+
+ #include "model.h"
+
+ void build_list() {
+
+ }
+
+ int get_frequency(string word_x, string word_y) {
+   return 0;
+ }
