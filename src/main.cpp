eebc32b5da93adc146d47cbc5d49eeddfb2cf8ed
Cameron Matson <cmatson@smu.edu>
2016-12-02 20:52:51 -0600

    move main to top

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,64 @@
 #define LYRICS_FILE "lyrics/lyrics.txt"
 #define TEST_FILE   "lyrics/test.txt"
 
+void rap(std::string bar, Model* model, Nouncer* nouncer, Denouncer* denouncer);
+void parseFile(std::string filename, Model* model, Nouncer* nouncer, Denouncer* denouncer);
+void parseLine(std::string in, Model* model, Nouncer* nouncer, Denouncer* denouncer);
+
+int main(int argc, char *argv[]) {
+  Model* model         = new Model();
+  Nouncer* nouncer     = new Nouncer();
+  Denouncer* denouncer = new Denouncer();
+
+  parseFile(LYRICS_FILE, model, nouncer, denouncer);
+
+  std::string bar;
+  std::cout << "Gimme a bar" << std::endl;
+  std::getline(std::cin, bar);
+
+  rap(bar, model, nouncer, denouncer);
+}
+
+/*
+ * Defines the recursive functionality to consume a given bar and respond.
+ */
+void rap(std::string bar, Model* model, Nouncer* nouncer, Denouncer* denouncer) {
+  if (bar == "exit") {
+    return;
+  }
+  else {
+    Battle* battle = new Battle(bar, model, nouncer, denouncer);
+    battle -> spit();
+
+    std::getline(std::cin, bar);
+    rap(bar, model, nouncer, denouncer);
+  }
+}
+
+/*
+ * Parses a text file of lyrics
+ *
+ * Iterates through each line of the file, passing references to
+ * the data structures to be constructed during analysis.
+ */
+void parseFile(std::string filename, Model* model, Nouncer* nouncer, Denouncer* denouncer) {
+  std::string line;
+  std::ifstream file(filename);
+
+  if (file) {
+    while(getline(file, line)) {
+      parseLine(line, model, nouncer, denouncer);
+    }
+
+    file.close();
+  }
+  else {
+    std::cerr << "Could not load lyrics file" << std::endl;
+  }
+
+  std::cout << "Model ready!" << std::endl;
+}
+
 /*
  * Parses a line
  *
@@ -74,58 +132,4 @@ void parseLine(std::string in, Model* model, Nouncer* nouncer, Denouncer* denoun
       }
     }
   }
-}
-
-/*
- * Parses a text file of lyrics
- *
- * Iterates through each line of the file, passing references to
- * the data structures to be constructed during analysis.
- */
-void parseFile(std::string filename, Model* model, Nouncer* nouncer, Denouncer* denouncer) {
-  std::string line;
-  std::ifstream file(filename);
-
-  if (file) {
-    while(getline(file, line)) {
-      parseLine(line, model, nouncer, denouncer);
-    }
-
-    file.close();
-  }
-  else {
-    std::cerr << "Could not load lyrics file" << std::endl;
-  }
-
-  std::cout << "Model ready!" << std::endl;
-}
-
-/*
- * Defines the recursive functionality to consume a given bar and respond.
- */
-void rap(std::string bar, Model* model, Nouncer* nouncer, Denouncer* denouncer) {
-  if (bar == "exit") {
-    return;
-  }
-  else {
-    Battle* battle = new Battle(bar, model, nouncer, denouncer);
-    battle -> spit();
-
-    std::getline(std::cin, bar);
-    rap(bar, model, nouncer, denouncer);
-  }
-}
-
-int main(int argc, char *argv[]) {
-  Model* model         = new Model();
-  Nouncer* nouncer     = new Nouncer();
-  Denouncer* denouncer = new Denouncer();
-
-  parseFile(LYRICS_FILE, model, nouncer, denouncer);
-
-  std::string bar;
-  std::cout << "Gimme a bar" << std::endl;
-  std::getline(std::cin, bar);
-
-  rap(bar, model, nouncer, denouncer);
 }
