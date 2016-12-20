c5d2a8877040a4b9537e2bb70d41203cb83d124f
Cameron Matson <cmatson@smu.edu>
2016-12-20 00:26:05 -0600

    word_test modlue update

--- test/word_test.cpp
+++ test/word_test.cpp
@@ -2,17 +2,45 @@
 
 #include "word.cpp"
 
-Word w("gravy", "su");
 
 TEST_CASE("initialize member variables") {
+  Word w("gravy", "su");
   CHECK(w.getVal() == "gravy");
   CHECK(w.getFrequency() == 1);
   CHECK(w.getStressPattern() == "su");
 
 }
 
+TEST_CASE("copy constructor") {
+  Word w("gravy", "su");
+
+  Word x = w;
+
+  CHECK(x.getVal() == "gravy");
+  CHECK(x.getFrequency() == 1);
+  CHECK(w.getStressPattern() == "su");
+
+  CHECK(&w != &x);
+}
+
 TEST_CASE("update frequency") {
+  Word w("gravy", "su");
   w.incrementFrequency();
 
   CHECK(w.getFrequency() == 2);
+}
+
+TEST_CASE("comparison operator") {
+  Word w("gravy", "su");
+  Word x("gravy", "su");
+
+  Word y("gravy", "uu");
+
+  Word z("notgravy", "su");
+  Word u("notgravy", "uu");
+
+  CHECK((w==x) == true);
+  CHECK((w==y) == true);
+  CHECK((w==z) == false);
+  CHECK((w==u) == false);
 }
